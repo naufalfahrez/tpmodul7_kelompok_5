f9dc5bd4ca1fe1af09790da1da553242c4233a97
mfaizadya <m.faiz.adya@gmail.com>
2025-04-07 19:58:38 +0700

    menambahkan class DataMahasiswa_103022300048

---
+++ DataMahasiswa_103022330048.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.IO;
+
+
+namespace Rakan
+{
+    public class Nama
+    {
+        public string depan { get; set; }
+        public string belakang { get; set; }
+    }
+
+    public class Mahasiswa
+    {
+        public Nama nama { get; set; }
+        public long nim { get; set; }
+        public string fakultas { get; set; }
+    }
+
+    public class DataMahasiswa_1030223330044
+    {
+        public void ReadJSON()
+        {
+            string filePath = "C:\\Users\\Faiz\\OneDrive - Telkom University\\Documents\\KS4\\KPL\\PRAKTIKUM\\tpmodul7_kelompok_5\\tp7_1_103022330044.json";
+            if (File.Exists(filePath))
+            {
+                string jsonString = File.ReadAllText(filePath);
+
+                Mahasiswa mhs = JsonSerializer.Deserialize<Mahasiswa>(jsonString);
+
+                Console.WriteLine($"Nama {mhs.nama.depan} {mhs.nama.belakang} dengan nim {mhs.nim} dari fakultas {mhs.fakultas}");
+            }
+            else
+            {
+                Console.WriteLine("File JSON tidak ditemukan.");
+            }
+        }
+    }
+}
